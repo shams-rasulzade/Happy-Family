4b851681a2aa8d26c74dc0a4eadf132cdf800ac8
Shams Rasulzade <rasulzade.sems@gmail.com>
2025-01-25 16:24:37 +0400

    - created Family class - added getter-setters for classes - implement new methods

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         String[][] schedule = new String[7][2];
@@ -15,17 +17,32 @@ public class Main {
         schedule[4][1] = "Play a Game or Do a Puzzle";
         schedule[5][1] = "Teach or Learn Something Together";
         schedule[6][1] = "Outdoor Adventure";
+
+
         Human mother = new Human("Padme", "Amidala", 1983);
         Human father = new Human("Anakin", "Skywalker", 1988);
         Pet pet = new Pet("dog", "Buddy", 4, 37, new String[]{"eat", "drink", "sleep"});
-        Human child = new Human("Luke", "Skywalker", 2008, 60, pet, father, mother, schedule);
-        child.describePet();
-        child.greetPet();
-        boolean b1 = child.feedPet(true);
+        Human child1 = new Human("Leia", "Skywalker", 2010, 70, schedule);
+
+        Family Skywalkers = new Family(mother, father);
+        Skywalkers.setPet(pet);
+        Skywalkers.describePet();
+        Skywalkers.greetPet();
+        boolean b1 = Skywalkers.feedPet(true);
+
+        Skywalkers.addChild(child1);
+
+        System.out.println("Child's family: " + child1.getFamily());
+
         System.out.println(mother);
         System.out.println(father);
-        System.out.println(child);
-        System.out.println(pet);
+
+        System.out.println("Before deletion: " + Arrays.toString(Skywalkers.getChildren()));
+        boolean isDeleted = Skywalkers.deleteChild(0);
+        System.out.println("Was the child deleted? " + isDeleted);
+        System.out.println("After deletion: " + Arrays.toString(Skywalkers.getChildren()));
+
+        System.out.println(Skywalkers.countFamily());
     }
 }
 
